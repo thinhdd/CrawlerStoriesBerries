e80135289ab47f122e4efbeaaddf646124333ee6
mr.thinhdaoduc@gmail.com <mr.thinhdaoduc@gmail.com>
2017-03-14 10:41:51 +0700

    update crawler

--- src/main/java/com/crawler/storiesberries/repository/CategoryRepository.java
+++ src/main/java/com/crawler/storiesberries/repository/CategoryRepository.java
@@ -1,10 +1,15 @@
 package com.crawler.storiesberries.repository;
 
 import com.crawler.storiesberries.model.Category;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by android on 3/7/2017.
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    @Query("select ca from Category as ca where ca.name=:name")
+    Category findByName(@Param("name") String type);
 }
--- src/main/java/com/crawler/storiesberries/service/impl/StoriesBerriesService.java
+++ src/main/java/com/crawler/storiesberries/service/impl/StoriesBerriesService.java
@@ -46,81 +46,96 @@ public class StoriesBerriesService implements IStoriesBerriesService {
             j += 1;
             String subUrl = element.attr("href");
             String type = element.childNode(0).toString();
-            Document subDoc = Jsoup.connect(subUrl).get();
-            Category category = new Category();
-            category.setName(type);
-            categoryRepository.save(category);
-            Elements pageElements = subDoc.select("a.page-numbers");
-            int countPage = Integer.parseInt(pageElements.get(pageElements.size() - 2).childNode(0).toString());
+            boolean tryCategory;
+            do {
+                try {
+                    Document subDoc = Jsoup.connect(subUrl).get();
+                    Category category = categoryRepository.findByName(type);
+                    if (category == null) {
+                        category = new Category();
+                        category.setName(type);
+                        categoryRepository.save(category);
+                    }
+                    Elements pageElements = subDoc.select("a.page-numbers");
+                    int countPage = Integer.parseInt(pageElements.get(pageElements.size() - 2).childNode(0).toString());
 
-            for (int page = 1; page < countPage; page++) {
-                boolean retryPage = false;
-                do {
-                    String pageUrl = subUrl + "page/" + page;
+                    for (int page = 1; page <= countPage; page++) {
+                        boolean retryPage;
+                        do {
+                            String pageUrl = subUrl + "page/" + page;
 
-                    System.out.println("page url " + pageUrl);
-                    try {
-                        Document pageDoc = Jsoup.connect(pageUrl).get();
-                        Elements elements = pageDoc.select("div.entry-image-inner a[href]");
-                        for (Element elementInPage : elements) {
-                            Thread.sleep(5000);
-                            String href = elementInPage.attr("href");
-                            System.out.println("story href " + href);
-                            String title = elementInPage.attr("title");
-                            String thumbnail = elementInPage.select("img").get(0).attr("src");
-                            boolean tryStory = false;
-                            do {
-                                try {
-                                    Document detailStoryDoc = Jsoup.connect(href).get();
-                                    String shortDes = detailStoryDoc.select("div.manual-excerpt").get(0).childNode(0).toString();
-                                    Elements elementByRegex = detailStoryDoc.select("div[itemprop=articleBody] p");
+                            System.out.println("page url " + pageUrl);
+                            try {
+                                Document pageDoc = Jsoup.connect(pageUrl).get();
+                                Elements elements = pageDoc.select("div.entry-image-inner a[href]");
+                                for (Element elementInPage : elements) {
+                                    String href = elementInPage.attr("href");
+                                    System.out.println("story href " + href);
+                                    String title = elementInPage.attr("title");
+                                    String thumbnail = elementInPage.select("img").get(0).attr("src");
                                     Story story = new Story();
                                     if (storyMap.containsKey(href)) {
                                         story = storyMap.get(href);
+                                        if (story.getCategories().contains(category)) {
+                                            continue;
+                                        }
                                         story.getCategories().add(category);
                                         story = storyRepository.save(story);
                                         storyMap.put(href, story);
                                         continue;
                                     }
-                                    story.setUrl(href);
-                                    story.setName(title);
-                                    story.setThumbnail(thumbnail);
-                                    story.setShortDes(shortDes);
-                                    if (story.getCategories() == null) {
-                                        story.setCategories(new ArrayList<Category>());
-                                    }
-                                    story.getCategories().add(category);
-                                    StringBuilder content = new StringBuilder();
-                                    for (Element dataContent : elementByRegex) {
-                                        Elements imgs = dataContent.select("img");
-                                        if (imgs.size() == 0) {
-                                            if (dataContent.childNodes().size() != 0) {
-                                                for (Node node : dataContent.childNodes()) {
-                                                    String str = node.toString();
-                                                    content.append(str);
+                                    boolean tryStory;
+                                    do {
+                                        try {
+                                            Document detailStoryDoc = Jsoup.connect(href).get();
+                                            String shortDes = detailStoryDoc.select("div.manual-excerpt").get(0).childNode(0).toString();
+                                            Elements elementByRegex = detailStoryDoc.select("div[itemprop=articleBody] p");
+                                            story.setUrl(href);
+                                            story.setName(title);
+                                            story.setThumbnail(thumbnail);
+                                            story.setShortDes(shortDes);
+                                            if (story.getCategories() == null) {
+                                                story.setCategories(new ArrayList<Category>());
+                                            }
+                                            story.getCategories().add(category);
+                                            StringBuilder content = new StringBuilder();
+                                            for (Element dataContent : elementByRegex) {
+                                                Elements imgs = dataContent.select("img");
+                                                if (imgs.size() == 0) {
+                                                    if (dataContent.childNodes().size() != 0) {
+                                                        for (Node node : dataContent.childNodes()) {
+                                                            String str = node.toString();
+                                                            content.append(str);
+                                                        }
+                                                    }
+                                                } else {
+                                                    content.append(imgs.get(0).attr("src"));
+                                                    content.append("@@@");
                                                 }
                                             }
-                                        } else {
-                                            content.append(imgs.get(0).attr("src"));
-                                            content.append("@@@");
+                                            story.setContent(content.toString());
+                                            story = storyRepository.save(story);
+                                            storyMap.put(title, story);
+                                            tryStory = false;
+                                        } catch (Exception e) {
+                                            tryStory = true;
+                                            Thread.sleep(10000);
                                         }
-                                    }
-                                    story.setContent(content.toString());
-                                    story = storyRepository.save(story);
-                                    storyMap.put(title, story);
-                                    tryStory=false;
-                                } catch (Exception e) {
-                                    tryStory = true;
-                                    Thread.sleep(10000);
+                                    } while (tryStory);
                                 }
-                            } while (tryStory);
-                        }
-                    } catch (Exception e) {
-                        retryPage = true;
-                        Thread.sleep(10000);
+                                retryPage = false;
+                            } catch (Exception e) {
+                                retryPage = true;
+                                Thread.sleep(10000);
+                            }
+                        } while (retryPage);
                     }
-                } while (retryPage);
-            }
+                    tryCategory = false;
+                } catch (Exception e) {
+                    tryCategory = true;
+                    Thread.sleep(10000);
+                }
+            } while (tryCategory);
         }
         Collection<Story> values = storyMap.values();
         storyRepository.save(values);
